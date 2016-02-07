fa43a819053445c159252b9baa3c753cb0d9ee1b
tobiaslandelius <tlandelius@gmail.com>
2016-02-07 15:03:50 +0100

    Did A2 by adding constructor to dictionary.cc and private variable unordered_set in dictionary.h, as well as created dicttest and added to Makefile

---
+++ lab2/dicttest.cc
@@ -0,0 +1,14 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+#include "dictionary.h"
+#include <unordered_set>
+
+
+using namespace std;
+
+int main() {
+	Dictionary *dict = new Dictionary();
+
+	delete dict;
+}
