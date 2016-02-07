7a6cdddb4d9247d86ca7d3447af4888026e1b14b
Erik Hedblom <Hedblom.e@gmail.com>
2016-02-07 20:07:47 +0100

    Implemented functions for A3, lab2

--- lab2/word.cc
+++ lab2/word.cc
@@ -1,15 +1,23 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 #include "word.h"
 
 using namespace std;
 
-Word::Word(const string& w, const vector<string>& t) {}
+Word::Word(const string &w, const vector<string> &t) : word(w), trigrams(t) {}
 
-string Word::get_word() const {
-	return string();
-}
+string Word::get_word() const { return word; }
 
-unsigned int Word::get_matches(const vector<string>& t) const {
-	return 0;
+unsigned int Word::get_matches(const vector<string> &t) const {
+  unsigned int res = 0;
+  auto start = trigrams.begin();
+  auto end = trigrams.end();
+  for (const string &s : t) {
+    start = find(start, end, s);
+    if (start == end)
+      break;
+    ++res;
+  }
+  return res;
 }
