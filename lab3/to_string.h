30f1155b09baf3a1d3f869a5b3395fb4fce6605e
tobiaslandelius <tlandelius@gmail.com>
2016-02-14 20:00:53 +0100

    A4 init

--- lab3/to_string.h
+++ lab3/to_string.h
@@ -1,22 +1,9 @@
-#ifndef DATE_H
-#define DATE_H
-#include <iostream>
+#ifndef TO_STRING_H
+#define TO_STRING_H
 
-class Date {
-	friend std::ostream& operator<<(std::ostream& os, const Date& d);
-	friend std::istream& operator>>(std::istream& is, Date& d);
-public:
-	Date();                    // today's date
-	Date(int y, int m, int d); // yyyy-mm-dd
-	int getYear() const;       // get the year
-	int getMonth() const;      // get the month
-	int getDay() const;        // get the day
-	void next();               // advance to next day
+#include <string>
 
-private:
-	int year;  // the year (four digits)
-	int month; // the month (1-12)
-	int day;   // the day (1-..)
-	static int daysPerMonth[12]; // number of days in each month
-};
+template <typename T>
+string to_string(const T& v1, const T& v2);
+
 #endif
