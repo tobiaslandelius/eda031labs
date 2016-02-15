7453ff46171dc73d61bfbdc02f1046e76035b083
tobiaslandelius <tlandelius@gmail.com>
2016-02-15 08:24:16 +0100

    deleted tostring

--- lab3/to_string.cc
+++
@@ -1,9 +0,0 @@
-#include <string> /* for swap */
-#include <iostream>
-
-using namespace std;
-
-template <typename T>
-string to_string(const T& v1, const T& v2) {
-	return "Hej";
-}
--- lab3/to_string.h
+++
@@ -1,9 +0,0 @@
-#ifndef TO_STRING_H
-#define TO_STRING_H
-
-#include <string>
-
-template <typename T>
-string to_string(const T& v1, const T& v2);
-
-#endif
